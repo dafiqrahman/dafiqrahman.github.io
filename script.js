ba13fb3ccb6110cfb3e78dd8017f5933542e5e83
dafiqrahman <46261435+dafiqrahman@users.noreply.github.com>
2022-05-05 16:57:48 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+$('.page-scroll').on('click', function(){
+    // ambil isi href
+    var tujuan = $(this).attr('href');
+    // tangkap elemen ybs
+    var alamatujuan =$(tujuan);
+
+    // pindahkan scroll
+    $('html,body').animate({
+        scrollTop: alamatujuan.offset().top - 50
+    });
+});
